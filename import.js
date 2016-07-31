bfb3fd606f034aa8ac88dc177be9f387bef50060
Kuukai <sinigami3427@gmail.com>
2016-07-31 23:48:02 +0900

    import的なやつ

---
+++ import.js
@@ -0,0 +1,77 @@
+var Thread=java.lang.Thread;
+var Runnable=java.lang.Runnable;
+var R=android.R;
+
+var Base64=android.util.Base64;
+var Bitmap=android.graphics.Bitmap;
+var BitmapFactory=android.graphics.BitmapFactory;
+var BitmapDrawable=android.graphics.drawable.BitmapDrawable;
+var Rect=android.graphics.Rect;
+var Paint=android.graphics.Paint;
+var Canvas=android.graphics.Canvas;
+var Color=android.graphics.Color;
+
+var NinePatchDrawable=android.graphics.drawable.NinePatchDrawable;
+var LayerDrawable=android.graphics.drawable.LayerDrawable;
+var ClipDrawable=android.graphics.drawable.ClipDrawable;
+var AnimationDrawable=android.graphics.drawable.AnimationDrawable;
+var StateListDrawable=android.graphics.drawable.StateListDrawable;
+var Shader=android.graphics.Shader;
+var Matrix=android.graphics.Matrix;
+var ColorMatrix=android.graphics.ColorMatrix;
+var ColorMatrixColorFilter=android.graphics.ColorMatrixColorFilter;
+var ColorDrawable=android.graphics.drawable.ColorDrawable;
+var ShapeDrawable=android.graphics.drawable.ShapeDrawable;
+var ColorStateList=android.content.res.ColorStateList;
+
+var Button=android.widget.Button;
+var ImageButton=android.widget.ImageButton;
+var ToggleButton=android.widget.ToggleButton;
+var Switch=android.widget.Switch;
+var CheckBox=android.widget.CheckBox;
+var OnClickListener=android.view.View.OnClickListener;
+var OnTouchListener=android.view.View.OnTouchListener;
+var MotionEvent=android.view.MotionEvent;
+var OnSeekBarChangeListener=android.widget.SeekBar.OnSeekBarChangeListener;
+var OnCheckedChangeListener=android.widget.CompoundButton.OnCheckedChangeListener;
+var LinearLayout=android.widget.LinearLayout;
+var RelativeLayout=android.widget.RelativeLayout;
+var FrameLayout=android.widget.FrameLayout;
+var ScrollView=android.widget.ScrollView;
+var ImageView=android.widget.ImageView;
+var TextView=android.widget.TextView;
+var PopupWindow=android.widget.PopupWindow;
+var EditText=android.widget.EditText;
+var SeekBar=android.widget.SeekBar;
+var ProgressBar=android.widget.ProgressBar;
+var RadioButton=android.widget.RadioButton;
+
+var View=android.view.View;
+var ViewGroup=android.view.ViewGroup;
+var Gravity=android.view.Gravity;
+
+var DisplayMetrics=android.util.DisplayMetrics;
+
+var Environment=android.os.Environment;
+
+var File=java.io.File;
+var FileInputStream=java.io.FileInputStream;
+var FileOutputStream=java.io.FileOutputStream;
+var FileWriter=java.io.FileWriter;
+var FileReader=java.io.FileReader;
+var BufferedWriter=java.io.BufferedWriter;
+var BufferedReader=java.io.BufferedReader;
+
+var TGALoader=net.zhuoweizhang.mcpelauncher.texture.tga.TGALoader;
+
+var ScriptManager=net.zhuoweizhang.mcpelauncher.ScriptManager;
+var ScriptableObject=org.mozilla.javascript.ScriptableObject;
+
+var ByteBuffer=java.nio.ByteBuffer;
+var ByteOrder=java.nio.ByteOrder;
+
+var DefaultDirectory=Environment.getExternalStorageDirectory().getAbsolutePath();
+
+var activity=com.mojang.minecraftpe.MainActivity.currentMainActivity.get();
+var context=com.mojang.minecraftpe.MainActivity.currentMainActivity.get();
+var ctx=com.mojang.minecraftpe.MainActivity.currentMainActivity.get();
